b7869351006f203b50e95a35ca99f3cc2eb5dcbb
edgarrobles1234 <edgarrobles1234@yahoo.com>
2023-09-12 10:10:14 -0700

    second commit

---
+++ components/layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ children, itIsHome }) {
+    return (
+        <div>
+            <Head>
+                <title>Basic Next App</title>
+            </Head>
+            <header>
+                <nav>
+                    <a href="https://www.youtube.com/watch?v=uRCK3umiKQQ">NFL QBs</a>
+                </nav>
+            </header>
+            <main>
+                {children}
+            </main>
+
+            {!itIsHome && (
+                <Link href="/" className="btn-btn-success mt-3">
+                    Going home!
+                </Link>
+            )
+            }
+            <footer>
+                <h4>
+                    FOOTER
+                </h4>
+            </footer>
+        </div>
+    );
+}
